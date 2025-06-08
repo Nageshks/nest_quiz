76a121cf787f7b764e0482c44b5518014829f9ad
nagesh <nageshempire@gmail.com>
2025-06-08 12:26:13 +0530

    handling cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:4200';
+const frontendUrl = process.env.FRONTEND_URL || null;
+
+const allowedOrigins = [
+  'http://localhost:4200',
+  'http://localhost:5173',
+  'https://quiz.nageshks.com',
+  ...(frontendUrl ? [frontendUrl] : []),
+];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: frontendUrl,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`Not allowed by CORS: ${origin}`));
+      }
+    },
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
